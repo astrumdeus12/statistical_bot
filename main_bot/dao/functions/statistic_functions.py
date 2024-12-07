f8f99fce1e56ffd340c7f0f78e16b76f202a2acf
astrumdeus12 <ghghuggvhb@gmail.com>
2024-12-07 19:13:22 +1000

    fixed days periods

--- main_bot/dao/functions/statistic_functions.py
+++ main_bot/dao/functions/statistic_functions.py
@@ -5,6 +5,18 @@ from datetime import date, timedelta
 from decimal import Decimal, ROUND_HALF_UP
 
 
+async def get_first_last_days(days):
+    first_day = date.today()
+    last_day = date.today()
+    if days == 7:
+        first_day = date.today() - timedelta(days=date.today().weekday())
+        last_day = first_day + timedelta(days=6)
+    elif days == 30:
+        first_day = date.today().replace(day=1)
+        last_day = (first_day + timedelta(days=31)).replace(day=1) - timedelta(days=1)
+    return [first_day, last_day]
+        
+
 async def count_premium(result : dict):
     credit_cards_prem = Decimal(result['total_credit_cards']) * Decimal(250)
     box_insurance_prem = Decimal(result['box_insurance']) * Decimal(5/100)
@@ -16,12 +28,42 @@ async def count_premium(result : dict):
     return total_prem.quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
     
 
-
+async def count_all_sells(first_day, last_day):
+    async for session in get_async_session():
+        async with session.begin():
+            users_sells = await session.execute(
+                select(
+                    func.sum(Sells.credits).label('total_credits'),
+                    func.sum(Sells.insurance).label('total_insurance'),
+                    func.sum(Sells.client_calls).label('total_client_calls'),
+                    func.sum(Sells.deb_cards).label('total_deb_cards'),
+                    func.sum(Sells.credit_cards).label('total_credit_cards'),
+                    func.sum(Sells.investition_insurance).label('total_investition_insurance'),
+                    func.sum(Sells.box_insurance).label('box_insurance'),
+                    func.sum(Sells.credit_count).label('total_credit_count'),
+                ).where(Sells.day >= first_day)
+                .where(Sells.day <= last_day))
+            
+            result = users_sells.fetchone()
+            result_dict = {}
+            if result:
+                result_dict.update({k: getattr(result, k) for k in result._mapping})
+            else:
+                result_dict = {'total_credits': 0, 'total_insurance': 0, 'box_insurance': 0, 'total_client_calls':0, 'total_deb_cards': 0, 'total_credit_cards': 0, 'total_investition_insurance': 0, 'period_credit_count': 0, 'total_credit_count': 0}
+                
+            result_answer = f'''\n Продажи за указанный период: \n Кредиты: {result_dict['total_credits'] or 0} \n Количество кредитов: {result_dict['total_credit_count']} \n Страховки: {result_dict['total_insurance'] or 0} \n Коробочные страхования: {result_dict['box_insurance']} \n Звонки: {result_dict["total_client_calls"] or 0} \n Дебетовые карты: {result_dict['total_deb_cards'] or 0} \n Кредитные карты: {result_dict["total_credit_cards"] or 0} \n Инвестиционное страхование жизни: {result_dict["total_investition_insurance"] or 0}'''
+            
+            return result_answer
+            
+            
+            
 
 async def get_user_sells_statistic(days:int, user_tg_id:str):
     async for session in get_async_session():
         async with session.begin():
-            time_delta = date.today() - timedelta(days=days)
+            days_list = await get_first_last_days(days)
+            first_day = days_list[0]
+            last_day = days_list[1]
             first_day_current_month = date.today().replace(day=1)
             last_day_current_month = (first_day_current_month + timedelta(days=31)).replace(day=1) - timedelta(days=1)
             user_sells = await session.execute(
@@ -38,7 +80,8 @@ async def get_user_sells_statistic(days:int, user_tg_id:str):
                     func.sum(Sells.credit_count).label('period_credit_count'),
                 ).join(User)  
                 .where(Sells.user_id == user_tg_id)  
-                .where(Sells.day > time_delta)
+                .where(Sells.day >= first_day)
+                .where(Sells.day <= last_day)
                 .group_by(Sells.user_id, User.name)  
             )
             result_period = user_sells.fetchone()  
@@ -59,8 +102,7 @@ async def get_user_sells_statistic(days:int, user_tg_id:str):
             if result_period:
                 result.update({k: getattr(result_period, k) for k in result_period._mapping})
                 result['total_credit_count'] = credits_count.total_credit_count if credits_count else 0
-                print(credits_count, credits_count is None)
-                print(result['total_credit_count'])
+                
             else:
                 result = {'user_name': 'Нет данных', 'total_credits': 0, 'total_insurance': 0, 'box_insurance': 0, 'total_client_calls':0, 'total_deb_cards': 0, 'total_credit_cards': 0, 'total_investition_insurance': 0, 'period_credit_count': 0, 'total_credit_count': 0}
 
@@ -82,7 +124,9 @@ async def get_user_sells_statistic(days:int, user_tg_id:str):
 async def get_all_sells_statistic(days:int):
     async for session in get_async_session():
         async with session.begin():
-            time_delta = date.today() - timedelta(days=days)
+            days_list = await get_first_last_days(days)
+            first_day = days_list[0]
+            last_day = days_list[1]
             first_day_current_month = date.today().replace(day=1)
             last_day_current_month = (first_day_current_month + timedelta(days=31)).replace(day=1) - timedelta(days=1)
             user_sells = await session.execute(
@@ -98,7 +142,8 @@ async def get_all_sells_statistic(days:int):
                     func.sum(Sells.box_insurance).label('box_insurance'),
                     func.sum(Sells.credit_count).label('period_credit_count'),
                 ).join(User)  
-                .where(Sells.day > time_delta)
+                .where(Sells.day >= first_day)
+                .where(Sells.day <= last_day)
                 .group_by(Sells.user_id, User.name))
             
             period_result = user_sells.fetchall()  
@@ -122,10 +167,10 @@ async def get_all_sells_statistic(days:int):
                 result.update({k: getattr(user, k) for k in user._mapping})
                 result['total_credit_count'] = 0
                 for counts in credits_count:
-                    print(counts.total_credit_count)
+                    
                     if counts.user_tg_id == result['user_tg_id']:
                         result['total_credit_count'] = counts.total_credit_count 
-                print(result)
+                
                 result['premium'] = await count_premium(result)
                 results.append(result)
                 
@@ -135,7 +180,7 @@ async def get_all_sells_statistic(days:int):
                 user['rank'] = rank
                 
                 
-            result_answer = ''
+            result_answer = await count_all_sells(first_day, last_day)
             second_result_answer = ''
             for result in sorted_users:
                 if result['rank'] < 10:
